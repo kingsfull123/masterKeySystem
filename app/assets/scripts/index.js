25371f2459f3a9aeeafc3adebe1a4d70d86c2a7f
Jason Yang <syan19@live.cn>
2020-02-20 22:26:48 +1100

    our start section

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,1 +1,14 @@
 import '../styles/styles.css';
+
+
+function Person(name, number) {
+    this.firstName = name;
+    this.favNumber = number;
+
+    this.greet = function() {
+        console.log('this is ' + this.firstName , 'my number is ' + this.favNumber)
+    }
+}
+
+const luke = new Person('luck', '15');
+luke.greet()
